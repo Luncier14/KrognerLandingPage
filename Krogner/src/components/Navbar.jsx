e9590f0c8227a5e9b03620b3e9620bcae9b185a9
Luncier14 <jonathanqp201@gmail.com>
2025-04-25 07:09:20 -0600

    Hero section flex
    Flex plex hero

--- Krogner/src/components/Navbar.jsx
+++ Krogner/src/components/Navbar.jsx
@@ -26,9 +26,9 @@ const Navbar = () => {
             </div>
 
             {/* Mobile menu button */}
-            <button>
+            <button onClick={() => setisMenuOpen(!isMenuOpen)} className='md:hidden p-2'>
                 {
-                    isMenuOpen ?  <HiX/> : <HiMenu/>
+                    isMenuOpen ?  <HiX className='size-6'/> : <HiMenu className='size-6'/>
                 }
             </button>
 
@@ -56,9 +56,31 @@ const Navbar = () => {
                 </button>
 
             {/* mobile menu */}
-
-
         </div>
+
+        {/* Mobile menu items */}
+        {
+            isMenuOpen && (
+                <div className='md:hidden bg-white border-t border-gray-100 py-4'>
+                    <div className='container mx-auto px-4 space-y-3'>
+                        {navLinks.map((link,index) => (
+                            <a 
+                            key={index}
+                            onClick={() => {
+                                setActiveLink(link.href);
+                                setisMenuOpen(false);
+                            }}
+                            className={`block text-sm font-medium py-2 ${activeLink === link.href ? "text-blue-600" : "text-gray-600 hover:text-gray-900"}`} href={link.href}>{link.label}</a>
+                        ))}
+
+                        <button className='w-full bg-purple-900 text-white px-6 py-2.5 rounded-lg hover:bg-purple-950 text-sm font-medium transition-all hover:shadow-lg hover:shadow-purple-100'>
+                            Contáctanos
+                        </button>
+
+                    </div>
+                </div>
+            )
+        }
     </nav>
   )
 }
