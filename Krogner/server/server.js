3887a593891d4e2a7c20f02931aeeff6d6597b58
Luncier14 <jonathanqp201@gmail.com>
2025-05-11 23:21:24 -0600

    Comment TLS

--- Krogner/server/server.js
+++ Krogner/server/server.js
@@ -19,9 +19,9 @@ const transporter = nodemailer.createTransport({
         user: process.env.SMTP_USER,
         pass: process.env.SMTP_PASS
     },
-    tls: {
-        rejectUnauthorized: false // Solo para desarrollo
-    }
+    // tls: {
+    //     rejectUnauthorized: false 
+    // }
 });
 
 // Verificación SMTP
